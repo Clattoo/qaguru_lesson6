f3ed53d7408205bfa9f2f0aee994ed28124892e2
Maxim Shlemin <clatto@yandex.ru>
2024-09-29 13:31:52 +0300

    Added first redaction for PageObject

---
+++ src/test/java/pages/RegistrationPage.java
@@ -0,0 +1,81 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import pages.components.CalendarComponent;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+
+public class RegistrationPage {
+
+    private SelenideElement firstNameInput = $("#firstName"),
+
+            lastNameInput = $("#lastName"),
+
+            userEmailInput = $("#userEmail"),
+
+            genderWrapper = $("#genterWrapper"),
+
+            userPhoneNumberInput = $("#userNumber"),
+
+            calendarInput = $("#dateOfBirthInput");
+
+    CalendarComponent calendarComponent = new CalendarComponent();
+
+    public RegistrationPage openPage() {
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        return this;
+    }
+
+    public RegistrationPage setFirstName(String value) {
+        firstNameInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setLastName(String value) {
+        lastNameInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setEmail(String value) {
+        userEmailInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setGender(String value) {
+        genderWrapper.$(byText(value)).click();
+
+        return this;
+    }
+
+    public RegistrationPage setPhoneNumber(String value) {
+       userPhoneNumberInput.setValue(value);
+
+        return this;
+    }
+
+    public RegistrationPage setDateOfBirth(String day, String month, String year) {
+        calendarInput.click();
+        calendarComponent.setDate(day, month, year);
+
+        return this;
+    }
+
+    public RegistrationPage checkResults (String key, String value) {
+
+        $(".table-responsive").$(byText(key)).parent()
+                .shouldHave(text(value));
+
+
+        return this;
+    }
+}
